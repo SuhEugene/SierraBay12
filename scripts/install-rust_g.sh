447d52ac87c69d300919e9b14767add296bc2ded
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-11-18 01:16:38 +0300

    Add rustg caching

--- scripts/install-rust_g.sh
+++ scripts/install-rust_g.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install dependencies
-sudo dpkg --add-architecture i386
-sudo apt update || true
-sudo apt remove -y libssl1.1:amd64 || true
-sudo apt install libgcc-s1:i386
-sudo apt install -o APT::Immediate-Configure=false libc6:i386
-wget http://ftp.de.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.1_1.1.1n-0+deb10u6_i386.deb
-sudo dpkg -i libssl1.1_1.1.1n-0+deb10u6_i386.deb
-
-# Download rust_g
 mkdir -p ~/.byond/bin
-wget -O ~/.byond/bin/librust_g.so "https://github.com/ss220-space/rust-g-tg/releases/download/2.0.0-ss220/librust_g.so"
+wget -O ~/.byond/bin/librust_g.so "https://github.com/${RUST_G_REPO}/releases/download/${RUST_G_VERSION}/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
