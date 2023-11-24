280edef9775a87381addd3b18019727a199d4800
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-11-24 11:31:07 +0300

    Letters

--- scripts/install-rust_g.sh
+++ scripts/install-rust_g.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! -f "~/.byond/bin/librust_g.so" ]; then
+if [ -f ~/.byond/bin/librust_g.so ]; then
+  echo "Using cached ~/.byond/bin/librust_g.so."
+else
   echo "~/.byond/bin/librust_g.so doesn't exist! Downloading..."
   mkdir -p ~/.byond/bin
   wget -O ~/.byond/bin/librust_g.so "https://github.com/${RUST_G_REPO}/releases/download/${RUST_G_VERSION}/librust_g.so"
@@ -9,5 +11,5 @@ fi
 
 chmod +x ~/.byond/bin/librust_g.so
 
-echo "Checking ~/.byond/bin/librust_g.so..."
+echo "LDD ~/.byond/bin/librust_g.so:"
 ldd ~/.byond/bin/librust_g.so
